3278b7d3d346150d8c18e2966b1863bcc3a57f19
lulu660 <115704757+Fishlulu1314@users.noreply.github.com>
2024-03-14 00:36:38 +0800

    第二天freemarker和minio学习,生成文章详情静态页面地址功能开发(功能代码暂时放在测试类中)

---
+++ heima-leadnews-test/freemarker-demo/src/main/java/com/heima/freemarker/controller/HelloController.java
@@ -0,0 +1,7 @@
+package com.heima.freemarker.controller;
+
+import com.heima.freemarker.entity.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
